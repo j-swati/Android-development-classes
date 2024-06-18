ef48b30c17abe0c0bf9d02dad5b7f6d73c9cad33
swati-153 <swati.jidaga@gmail.com>
2024-06-18 17:26:56 +0530

    implemented Slider - updated MainActivity.kt

--- Compose_practice/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ Compose_practice/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,78 +1,44 @@
 package com.example.myapplication
 
-import android.app.Activity
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material3.Button
-import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import com.example.myapplication.ui.theme.MyApplicationTheme
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyApplicationTheme {
-                LinearDeterminateIndicator()
-            }
-        }
-    }
-}
-
-@Composable
-fun LinearDeterminateIndicator() {
-    var currentProgress by remember { mutableStateOf(0f) }
-    var loading by remember { mutableStateOf(false) }
-    val animatedProgress by animateFloatAsState(targetValue = currentProgress)
-    val scope = rememberCoroutineScope() // Create a coroutine scope
-
-    Column(
-        verticalArrangement = Arrangement.spacedBy(12.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier.fillMaxWidth()
-    ) {
-        Button(onClick = {
-            loading = true
-            scope.launch {
-                loadProgress { progress ->
-                    currentProgress = progress
-                }
-                loading = false // Reset loading when the coroutine finishes
-            }
-        }, enabled = !loading) {
-            Text("Start loading")
-        }
-
-        if (loading) {
-            LinearProgressIndicator(
-                progress = animatedProgress,
+            Column(
                 modifier = Modifier.fillMaxWidth(),
-            )
+            ) {
+                SliderMinimalExample()
+            }
         }
     }
 }
 
-/** Iterate the progress value */
-suspend fun loadProgress(updateProgress: (Float) -> Unit) {
-    for (i in 1..100) {
-        updateProgress(i.toFloat() / 100)
-        delay(100)
+@Preview
+@Composable
+fun SliderMinimalExample() {
+    var sliderPosition by remember { mutableStateOf(0f) }
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+    ) {
+        Slider(
+            value = sliderPosition,
+            onValueChange = { sliderPosition = it }
+        )
+        Text(text = "Slider Position: $sliderPosition")
     }
 }
