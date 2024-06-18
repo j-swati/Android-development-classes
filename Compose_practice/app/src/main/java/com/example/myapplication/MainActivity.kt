ea171abc91d8b98bf34b1e3d966559c8e2281a91
swati-153 <swati.jidaga@gmail.com>
2024-06-18 17:41:24 +0530

    implemented Cards - updated MainActivity.kt

--- Compose_practice/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ Compose_practice/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,17 +3,18 @@ package com.example.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material3.Slider
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,23 +23,47 @@ class MainActivity : ComponentActivity() {
             Column(
                 modifier = Modifier.fillMaxWidth(),
             ) {
-                SliderMinimalExample()
+                CardColumn()
             }
         }
     }
 }
 
-@Preview
 @Composable
-fun SliderMinimalExample() {
-    var sliderPosition by remember { mutableStateOf(0f) }
+fun CardColumn() {
     Column(
-        modifier = Modifier.fillMaxWidth(),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
     ) {
-        Slider(
-            value = sliderPosition,
-            onValueChange = { sliderPosition = it }
+        ElevatedCardExample("Card 1", "This is the first card.")
+        ElevatedCardExample("Card 2", "This is the second card.")
+        ElevatedCardExample("Card 3", "This is the third card.")
+    }
+}
+
+@Composable
+fun ElevatedCardExample(title: String, text: String) {
+    ElevatedCard(
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 6.dp
+        ),
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(100.dp)
+    ) {
+        Text(
+            text = title,
+            modifier = Modifier
+                .padding(16.dp),
+            textAlign = TextAlign.Center,
         )
-        Text(text = "Slider Position: $sliderPosition")
+        Text(
+            text = text,
+            modifier = Modifier
+                .padding(16.dp),
+            textAlign = TextAlign.Center,
+        )
     }
 }
